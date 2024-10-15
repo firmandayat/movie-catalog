85b30cfdbc5b84aa412bdaed37133ec49efd8b85
firmandayat <firman.txt@gmail.com>
2024-10-15 23:38:06 +0700

    first

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+const CONFIG = {
+  KEY: '558b3151bd6a8b31dd2e2d57a03be18e',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
